326be3bc2864213b40b74e12131ef6d53f37cbaa
ashleysnikers-ship-it <ashleysnikers@gmail.com>
2026-05-28 13:37:52 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -234,7 +234,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, terima_teks))
 
     logger.info("Bot polling dimulai...")
-    app.run_polling(allowed_updates=Update.ALL_TYPES)
+    app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
